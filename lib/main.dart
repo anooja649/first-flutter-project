f1b87359f0b5057bc3e1cc84ebdb88669f396193
anoojag649 <anoojag649@gmail.com>
2022-12-11 14:04:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 80, 98, 107),
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          centerTitle: true,
+          title: Text("I Am Poor"),
+        ),
+        // ignore: prefer_const_constructors
+        body: Center(
+          child: Image(
+            image: AssetImage('image/bp.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
